55cdec84085db964139a6f57bf1f05c2f6959a0a
Your Name <you@example.com>
2023-03-17 08:22:22 -0700

    Using put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - prints "Programming is like building a multilingual puzzle, followed by a new line"
@@ -6,9 +6,9 @@ include <stdio.h>
  */
 
 int main(void)
-(
-	puts("/"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	
 	return (0);
-)
+}
 
